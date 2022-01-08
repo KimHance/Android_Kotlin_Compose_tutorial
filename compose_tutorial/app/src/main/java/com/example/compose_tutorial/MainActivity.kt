a70303d178a496c1b9dd85852e280492e6ad5606
Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-01-08 18:23:48 +0900

    Compose UI tutorial(3)
    about Shape

--- compose_tutorial/app/src/main/java/com/example/compose_tutorial/MainActivity.kt
+++ compose_tutorial/app/src/main/java/com/example/compose_tutorial/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,8 +15,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.example.compose_tutorial.ui.theme.Compose_tutorialTheme
 import kotlin.random.Random
@@ -25,8 +32,9 @@ class MainActivity : ComponentActivity() {
             Compose_tutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    BoxWithConstraint()
+                    //BoxWithConstraint()
                 //Container()
+                    ShapeContainer()
                 }
             }
         }
@@ -36,7 +44,7 @@ class MainActivity : ComponentActivity() {
 // arrangement 요소를 어떤식으로 배열할지
 // arrangement 는 Row, Column 같은 요소들이 들어가는
 // 컨테이너성격의 컴포저블에서 요소들의 아이템을 정렬할 때 사용
-
+//
 // horizontal Arrangement 는 Start, End, Center만 존재
 // .SpaceBetween : 공간 모두 차지
 // .Start : 왼쪽으로
@@ -44,33 +52,33 @@ class MainActivity : ComponentActivity() {
 // .SpaceAround : 빈 공간을 남겨두기
 // .Center : 가운데
 // .SpaceEvenly : 요소들 사이에 공간을 똑같이 하기
-
+//
 // alignment 는 해당 컨테이너 안에 들어있는 요쇼들의 위치 설정
 // linearLayout 에서 gravity 와 동일
 // .Bottom : 아래에
 // .Top : 위에
 // .CenterVertically : 컨테이너의 수직 방향으로 중앙에
-
+//
 // Row 컴포저블 안에서 align 이 들어가기 때문에 CenterVertically
-
-
+//
+//
 // 박스는 겹칠 수 있음
 // 기존 relative, constraint, frame 레이아웃에서 뷰를 겹치는 것과 비슷
 // 아래로 내려갈수록 위에 뷰를 올리는 방식
 // alignment 는 row, column 보다 다양하게 지원
-
+//
 // .BottomCenter : 컨테이너의 중앙 아래
 // .BottomEnd : 컨테이너 아래 오른쪽
 // .BottomStart : 컨테이너의 아래 왼쪽
-
+//
 // .Center : 컨테이너 정중앙
 // .CenterStart : 컨테이너 중앙 왼쪽
 // .CenterEnd : 컨테이너의 중앙 오른쪽
-
+//
 // .TopCenter : 컨테이너의 위 중앙
 // .TopStart : 컨테이너의 위 왼쪽
 // .TopEnd : 컨테이너의 위 오른쪽
-
+//
 // propagateMinConstraints 해당 옵션을 true로 하면
 // 박스 안에 있는 제일 작은 크기의 뷰를 컨테이너 박스의 크기만큼 constraint 를
 
@@ -181,11 +189,47 @@ fun DummyBox(modifier : Modifier = Modifier, color : Color? =null){
     )
 }
 
+@Composable
+fun ShapeContainer(){
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        Alignment.CenterHorizontally
+    ) {
+        DummyBox(modifier = Modifier.clip(RectangleShape))  // 네모
+        DummyBox(modifier = Modifier.clip(CircleShape))   // 동그라미
+        DummyBox(modifier = Modifier.clip(RoundedCornerShape(10.dp)))  // 둥근 모서리
+        DummyBox(modifier = Modifier.clip(CutCornerShape(30.dp)))  // 잘린 모서리
+        DummyBox(modifier = Modifier.clip(TriangleShape()))
+    }
+}
+
+class TriangleShape() : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        val path = Path().apply {      // 파이썬의 터틀 그래픽과 비슷
+            moveTo(size.width / 2f, 0f)
+            lineTo(size.width, size.height)
+            lineTo(0f, size.height)
+            close()
+        }
+        return Outline.Generic(path = path)
+    }
+
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Compose_tutorialTheme {
-        BoxWithConstraint()
+        //BoxWithConstraint()
     //Container()
+        ShapeContainer()
     }
 }
